632d974e641c20697e6862a121839b62914d5498
Tahamikashif <165390371+Tahamikashif@users.noreply.github.com>
2024-04-01 05:01:43 +0500

    Add files via upload
    This is a cli number guessed game

---
+++ app.js
@@ -0,0 +1,16 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+const user = await inquirer.prompt([
+    {
+        message: 'please guess a number between 1-10',
+        type: "number",
+        name: "guessed_number",
+    }
+]);
+if (user.guessed_number === randomnumber) {
+    console.log("congratulation you are win");
+}
+else {
+    console.log("try again");
+}
